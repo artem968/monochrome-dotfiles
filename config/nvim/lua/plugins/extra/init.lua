335f48804771fcc909d6490d430313edffb8b689
artem968 <artemturenko98@gmail.com>
2025-10-08 21:10:54 +0200

    Update init.lua fix unresolved black-metal

--- config/nvim/lua/plugins/extra/init.lua
+++ config/nvim/lua/plugins/extra/init.lua
@@ -1,5 +1,5 @@
 return {
     { import = "plugins.extra.alpha" },  -- tell lazy.nvim to load alpha.lua
-    { import = "plugins.extra.black-metal" },
+    -- { import = "plugins.extra.black-metal" },
     -- any other extra plugins can go here
 }
